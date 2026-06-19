750b01b18c313a87d5b089f74f8a6dd49aff9305
Akshatha Boini <akshathaboini@gmail.com>
2026-06-19 13:01:20 -0400

    Update CORS for production frontend

--- jobsync-backend/src/main/java/com/jobsync/jobsync_backend/controller/JobApplicationController.java
+++ jobsync-backend/src/main/java/com/jobsync/jobsync_backend/controller/JobApplicationController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/applications")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://jobsync-kappa.vercel.app", "https://jobsync-tau.vercel.app"})
 @RequiredArgsConstructor
 public class JobApplicationController {
 
